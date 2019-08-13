78ff9a8cea96889ad4dc05953702351b8db1b4ad
najamshah <53971636+najamshah@users.noreply.github.com>
2019-08-13 14:14:54 +0400

    Complete Sign in Page with FormInput and Custom Button

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import SignIn from '../../components/sign-in/sign-in.component'
+import './sign-in-and-sign-up.styles.scss'
+const SignInAndSignUp = ()=> (
+    <div>
+        <SignIn></SignIn>
+    </div>
+)
+export default SignInAndSignUp
