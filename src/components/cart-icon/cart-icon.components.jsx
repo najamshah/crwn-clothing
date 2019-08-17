b5568d54631b0c365de8265c3fb87f6dc6bc2082
najamshah <53971636+najamshah@users.noreply.github.com>
2019-08-17 20:39:52 +0400

    Accumalating Quantity

--- src/components/cart-icon/cart-icon.components.jsx
+++ src/components/cart-icon/cart-icon.components.jsx
@@ -7,15 +7,19 @@ import { ReactComponent as ShoppingIcon } from '../../assests/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden , itemsCount}) => (
   <div className='cart-icon' onClick={toggleCartHidden} >
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemsCount}</span>
   </div>
 );
 
+const mapStateToProps = ({cart: {cartItems}})=>(
+  {itemsCount: cartItems.reduce((accumalatedQuanity, cartItem) => accumalatedQuanity+cartItem.quantity
+    ,0)}
+)
 const mapDispatchToProps = (dispatch)=>({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null,mapDispatchToProps) (CartIcon);
+export default connect(mapStateToProps,mapDispatchToProps) (CartIcon);
